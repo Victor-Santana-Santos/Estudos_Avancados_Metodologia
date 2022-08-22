9a98ca4831826db3c9cfed6ccbfaa3ed526d38ea
Victor Santos <victor.santanasantos@ufpe.br>
2022-08-22 16:00:30 -0300

    exercício de funções

---
+++ exercicio_funcoes.R
@@ -0,0 +1,15 @@
+# EXERCÍCIO FUNÇÕES
+
+f <- function(nro) {
+  if(nro < 0) {
+      cat("Esquerda\n")
+    } else {
+    cat("Direita\n")
+  }
+}
+
+duplicate <- function(x) {
+  x*2
+}
+
+duplicate(34954)
