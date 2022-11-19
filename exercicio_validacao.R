b48df2b60fe6457c5d24ecd072d5e4c64015c493
Victor Santos <victor.santanasantos@ufpe.br>
2022-11-19 18:01:01 -0300

    Exercício sobre validação

---
+++ exercicio_validacao.R
@@ -0,0 +1,19 @@
+# EXERCÍCIO: VALIDAÇÃO
+
+pacman::p_load(data.table, dplyr, tidyverse, validate)
+
+general_data <- fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+north_american_countries <-c("United States", "Canada", "Mexico") # vetor que identifica países latino americanos
+
+north_america <- general_data %>% filter(location %in% north_american_countries) # filtra casos apenas no vetor
+
+north_america <- north_america %>% select(location, new_cases, new_deaths)
+
+regras_north_america <- validator(new_cases >= 0, new_deaths >= 0)
+
+validacao_north_america <- confront(north_america, regras_north_america)
+
+summary(validacao_north_america)
+
+plot(validacao_north_america)
